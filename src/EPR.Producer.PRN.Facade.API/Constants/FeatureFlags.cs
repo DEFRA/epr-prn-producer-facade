cd1b53c6f51e335da1f0ecb8b25fcbd207eff599
ravi.sharma <ravi.sharmagautam@eviden.com>
2024-06-27 09:58:04 +0100

    removed default endpoint

--- src/EPR.Producer.PRN.Facade.API/Constants/FeatureFlags.cs
+++ src/EPR.Producer.PRN.Facade.API/Constants/FeatureFlags.cs
@@ -2,7 +2,5 @@
 {
     public static class FeatureFlags
     {
-        public const string UpdateWeatherForecast = "UpdateWeatherForecast";
-        public const string AdditionalFeature = "AdditionalFeature";
     }
 }
